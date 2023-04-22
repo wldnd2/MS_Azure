a9463a131f6b7153c69411510f5af0c923aae6a0
wldnd2 <jiwoong000628@gmail.com>
2023-04-23 02:15:26 +0900

    refactor: Modify file name code

--- pdf.py
+++ pdf.py
@@ -8,10 +8,15 @@ def pdf_processing(filename:str, start_page, end_page, num_of_questions):
     """ PDF 추출 Setting """
     reader = PdfReader("./uploads/" + filename)
     pages = reader.pages 
-    txt = open("./result/" + filename + "_questions.txt", 'w', encoding='utf-8')
+    txt = open("./result/" + filename.split(".")[0] + "_questions.txt", 'w', encoding='utf-8') # filename.split(".")[0] => 확장자명 제거
     cur = 0 # 현재 페이지
+
+    """ JSON Setting """
+    json_data = {}
+    json_data['questions'] = []
+
     """ ChatGPT Setting """
-    OPEN_AI_API_KEY = "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" # 각자 키 입력 (https://platform.openai.com/account/api-keys 확인 ㄱ)
+    OPEN_AI_API_KEY = "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" # 각자 키 입력 (https://platform.openai.com/account/api-keys 확인 ㄱ)
     openai.api_key = OPEN_AI_API_KEY
     model = "gpt-3.5-turbo"
     messages = [ # system content 손 볼 필요 있음
@@ -19,15 +24,10 @@ def pdf_processing(filename:str, start_page, end_page, num_of_questions):
                 답변 형식은 json형식으로 각각의 문제번호(숫자로만 key를 표시), 문제(question을 key로 표시), \
                 4지선다(1,2,3,4를 key로 한다), 정답(answer를 key로 표시)과 해설(explanation를 key로 표시)을 알려주세요."}
     ]
-    
-    """ JSON Setting """
-    json_file_path = "./downloads/questions.json"
-    json_data = {}
-    json_data['questions'] = []
 
-    for page in pages: # 페이지별 문제 추출
+    """ 페이지 추출 """
+    for page in pages:
         cur += 1 # 현재 페이지 수
-        #print('end============',type(end_page))
         if cur < int(start_page) or cur > int(end_page): # start_page ~ end_page 까지만 작동
             continue
         
@@ -44,12 +44,13 @@ def pdf_processing(filename:str, start_page, end_page, num_of_questions):
         
         # Json 데이터에 현재 페이지에서 생성된 question 추가
         """ !!Json 형식으로 GPT 답변이 온다고 가정하고 코드 작성!! """
-#        add_code = "json_data['questions'].append(" + answer + ")"
-#        exec(add_code)
+        # add_code = "json_data['questions'].append(" + answer + ")"
+        # exec(add_code)
+        
+        """ json 데이터 추출 """
         print(f"answer: {answer}")
         # JSON 문자열을 파싱하여 딕셔너리로 저장
         data = json.loads(answer)
-
         # 문제 형식으로 출력
         for key, value in data.items():
             txt.write(f'{value["question"]}\n')
@@ -57,15 +58,10 @@ def pdf_processing(filename:str, start_page, end_page, num_of_questions):
                 txt.write(f'{i}. {value[str(i)]}\n')
             txt.write(f'정답: {value["answer"]}\n')
             txt.write(f'해설: {value["explanation"]}\n\n')
-        # txt.write(answer + "\n")
-
-        with open("./result/" + filename + "_questions.json", "w", encoding="utf-8") as f:
+        
+        """  Json Data 입력 """
+        with open("./result/" + filename.split(".")[0] + "_questions.json", "w", encoding="utf-8") as f:
             json.dump(answer,f, ensure_ascii=False, sort_keys=True)
-
         # 다음 페이지를 위해 messages에서 현재 페이지의 text로 작성된 user content 삭제
         messages.pop()
-        
-    # Json 파일에 쓰기
-    # with open(json_file_path, 'w', encoding='utf-8') as outfile:
-    #     json.dump(json_data, outfile, indent='\t', ensure_ascii=False)
     txt.close()
