12345c50a698b161f96fa8080f5b7933ec93f2ad
ruth <kimminju77786@gmail.com>
2023-04-24 13:34:09 +0900

    debug

--- run.py
+++ run.py
@@ -1,51 +1,88 @@
-import os
-from werkzeug.utils import secure_filename
-from flask import Blueprint, send_file, request, redirect, url_for, render_template, Flask
-from PyPDF2 import PdfReader
-import openai, json
-
-def pdf_processing(filename:str, start_page, end_page, num_of_questions_per_page):    
-    """ PDF 추출 Setting """
-    reader = PdfReader("./uploads/" + filename)
-    pages = reader.pages 
-    # txt = open("./downloads/" + filename + "_questions.txt", 'w', encoding='utf-8')
-    questions = {}
-    questions_number = 1
-    cur_page = 0 # 현재 페이지
-    """ ChatGPT Setting """
-    OPEN_AI_API_KEY = "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" # 각자 키 입력 (https://platform.openai.com/account/api-keys 확인 ㄱ)
-    openai.api_key = OPEN_AI_API_KEY
-    model = "gpt-3.5-turbo"
-    messages = [ # system content 손 볼 필요 있음
-            {"role": "system", "content": "사용자가 전송하는 내용을 토대로 문제를 정확히 1개만 출제해. { 문제 : 질문, 1 : 첫 번째 선택지, 2 : 두 번째 선택지, 3: 세 번째 선택지, 4: 네 번째 선택지, 정답: 정답번호, 해설: 해설 } 이러한 json형태로 출력해."}
-    ]
+# -*- coding: utf-8 -*-
+"""
+    pip install flask
+    pip install openai -> 오류나면 cmd 관리자권한으로 실행해서 입력
+    pip install PyPDF2
     
-    """ JSON Setting """
-    json_file_path = "./downloads/questions.json"
-    json_data = {}
-    json_data['questions'] = []
+    실행: python run.py
+"""
+import os
+import json
+import openai
+import secrets
+from PyPDF2 import PdfReader
+from pdf import pdf_processing
+from werkzeug.utils import secure_filename
+from flask import Blueprint, send_file, request, redirect, url_for, render_template, Flask, session
 
-    for page in pages: # 페이지별 문제 추출
-        cur_page += 1 # 현재 페이지 수
-        if cur_page < int(start_page) or cur_page > int(end_page): # start_page ~ end_page 까지만 작동
-            continue
-        
-        query = page.extract_text() # 각 페이지에서 text 추출하여 query에 저장
-        print(f"-- {cur_page} 페이지 문제 추출 중 --")
-        messages.append({"role": "user", "content": query})
-        
-        for i in range(int(num_of_questions_per_page)):
-            # ChatGPT API 호출하기
-            response = openai.ChatCompletion.create(
-                model=model,
-                messages=messages
-            )
-            answer = response['choices'][0]['message']['content']
-            print('answer: ',answer)
-            questions[questions_number] = answer
-            questions_number += 1
+app = Flask(__name__)
+app.secret_key = secrets.token_hex(16)
+app.jinja_env.add_extension('jinja2.ext.loopcontrols')
 
-        # 다음 페이지를 위해 messages에서 현재 페이지의 text로 작성된 user content 삭제
-        messages.pop()
-    # txt.close()
-    return 
+UPLOAD_FOLDER = os.getcwd() + '/uploads'  # 절대 파일 경로
+ALLOWED_EXTENSIONS = set(['pdf'])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 8 * 1024 * 1024 # 16MB로 업로드 크기 제한, RequestEntityTooLarge : 크기 초과시 이 예외 발생, 처리 필요
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        result = request.form # start_page, end_page, num_of_questions 변수 저장
+        file = request.files['myfile']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            session['filename'] = filename
+            session['result'] = result
+            return redirect(url_for('questions'))
+    return render_template('fileupload.html') # GET 
+
+@app.route('/questions', methods=['GET', 'POST'])
+def questions():
+    print(f"***************{request.method}*****************")
+    if request.method == 'POST':
+        return redirect(url_for('check'))
+    else:
+        filename = session.get('filename')
+        result = session.get('result')
+        # if request.method == 'POST':
+            # res = request.form # filename, start_page, end_page, num_of_questions 변수 저장
+        result["filename"] = filename
+        # question = pdf_processing(result["filename"], result["start_page"], result["end_page"], result["num_of_questions"])
+        # Qna_result = {}
+        # Qna_number = 1
+        # # print("***********{ PDF RESULT }***********")
+        # # print(question)
+        # print("\n***********{ Questions PROCESSING }*************")
+        # for key, value in question.items():
+        #     try:
+        #         if(value[0] == "[" and value[-1] == "]"):
+        #             print("***********{ List processing }***********")
+        #             value = json.loads(value)
+        #             for item in value:
+        #                 Qna_result[Qna_number] = item
+        #                 Qna_number += 1
+        #         else:
+        #             print("***********{ Json processing }***********")
+        #             Qna_result[Qna_number] = json.loads(value)
+        #             Qna_number += 1
+        #     except:
+        #         print("***********{ PASS!!!! }*************")
+        #         continue
+        # print("\n***********{ FINAL RESULT }*************")
+        # print(Qna_result)
+        # print("*******************************************")
+        Qna_result = {1: {'문제': '지식 재산권과 관련된 내용 중 특허권에 대한 설명은?', '1': '자연법칙을 이용한 기술적 사상의 창작으로서 발명수준이 고도한 발명에 대하여 부여되는 권리', '2': '물품의 형상, 구조, 조합에 관한 고안에 대해여 부여되는 권리', '3': '물품의 외관인 디자인에 대한 아이디어를 보호하는 권리', '4': '식별력있는 상표또는 서비스표에 부여되는 권리', '정답': 1, '해설': '특허권은 자연법칙을 이용한 기술적 사상의 창작으로서 발명수준이 고도한 발명에 대하여 부여되는 권리입니다.'}}
+        return render_template('questions.html', result=Qna_result)
+
+@app.route('/check', methods=['GET', 'POST'])
+def check():
+    return render_template('check.html')
+
+if __name__ == '__main__':
+    app.run(debug=True) # 배포시 debug=True 삭제
+    # app.run(host='0.0.0.0') 배포 시 사용
